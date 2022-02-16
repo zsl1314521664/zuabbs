92ab62a50d40a69e6db24fb5318f3b902f2a04da
zsl1314521664 <1977932918@qq.com>
2022-02-16 10:42:43 +0800

    移动User模型到app/models

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://zuazsl.top" target="_blank">爱学习的赵同学</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="1977932918@qq.com">联系作者</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title','ZUABBS')-在郑航校园生活服务平台</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @include('shared._messages')
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
