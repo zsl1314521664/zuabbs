67f2802d8bdb97567b22b7093284df238fdd0b21
zsl1314521664 <1977932918@qq.com>
2022-02-17 13:06:20 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,10 +20,20 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 //    更新个人信息
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
 //        dd($request->all());
-        $user->update($request->all());
+//        dd($request->avatar);
+        $data=$request->all();
+//        dd($data);
+        if($request->avatar){
+            $result=$uploader->save($request->avatar,'avatars',$user->id);
+        }
+        if($result){
+            $data['avatar']=$result['path'];
+        }
+//        $user->update($request->all());
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 }
