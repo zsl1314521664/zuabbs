2267575ab501798f7e8137af9b234c93348ba157
zsl1314521664 <1977932918@qq.com>
2022-02-20 16:32:34 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,31 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query,$order)
+    {
+//        不同的排序使用不同的数据读取逻辑
+        switch ($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+//        预防N+1问题
+        return $query->with('user','category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+//        按更新时间排序
+        return $query->orderBy('updated_at','desc');
+    }
+
+    public function scopeRecent($query)
+    {
+//        按创建时间排序
+        return $query->orderBy('created_at','desc');
+    }
 }
