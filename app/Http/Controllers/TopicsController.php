8aa5c900a9fa5b8ac74dc34f0fb50735422cbb58
zsl1314521664 <1977932918@qq.com>
2022-03-06 21:46:44 +0800

    删除个人话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -63,8 +63,9 @@ class TopicsController extends Controller
 	{
 		$this->authorize('destroy', $topic);
 		$topic->delete();
+		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功！');
 	}
 //	文本框上传图片
     public function uploadImage(Request $request,ImageUploadHandler $uploader)
